fd27b85b37b3665f96434f85c5b43257803ede62
Anvay Bhanap <anvay.bhanap@gmail.com>
2024-03-07 19:37:08 -0800

    completed API, starting frontent

---
+++ frontend/src/pages/DeleteBooks.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function DeleteBooks() {
+  return (
+    <div>DeleteBooks</div>
+  )
+}
+
+export default DeleteBooks
---
+++ frontend/src/pages/ShowBook.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function ShowBook() {
+  return (
+    <div>ShowBook</div>
+  )
+}
+
+export default ShowBook
