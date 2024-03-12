6169704b62320c6d74b32f4bec82b1bac21f1d40
Anvay Bhanap <anvay.bhanap@gmail.com>
2024-03-12 03:32:48 -0700

    added Card view for books

---
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle, BiShow } from 'react-icons/bi';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineDelete } from 'react-icons/md';
+import { useState } from 'react';
+// import BookModal from './BookModal';
+
+function BookSingleCard({ book }) {
+  // const [showModal, setShowModal] = useState(false);
+
+  return (
+    pass
+  );
+}
+
+export default BookSingleCard
