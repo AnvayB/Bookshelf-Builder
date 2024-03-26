4aa43f654f9b1caf2dc189eef74f8878a9c9c59c
Anvay Bhanap <anvay.bhanap@gmail.com>
2024-03-26 01:42:57 -0700

    final edits

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+
 import { Link } from "react-router-dom"
-import './Intro.css'
+ import './Intro.css'
 
 function Intro() {
+
   return (
     <div className='intro'>
 
-    <Link to={'/home'}>
-    <button className='view'><p>View Books</p></button>
-    </Link>
+      <Link to={'/home'}>
+        <button className='view'><p>View Books</p></button>
+      </Link>
     </div>
   )
 }
 
 export default Intro
+
+// import { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import './Intro.css';
+
+// function Intro() {
+//   const [redirect, setRedirect] = useState(false);
+
+//   const handleClick = () => {
+//     setRedirect(true); // Set redirect to true when button is clicked
+//   };
+
+//   return (
+//     <div className={`intro ${redirect ? 'slide-up' : ''}`}>
+//       {redirect && <Link to="/home" />} {/* Redirect to "/home" if redirect is true */}
+//       <button className="view" onClick={handleClick}>
+//         <p>View Books</p>
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default Intro;
